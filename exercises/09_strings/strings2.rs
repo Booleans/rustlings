89dd9c5b71c54b7d022dc5e0de80cd740248260d
Andrew Nicholls <andrew.nicholls@databricks.com>
2025-10-24 14:05:24 -0700

    Finished Strings Section

--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -1,12 +1,15 @@
 // TODO: Fix the compiler error in the `main` function without changing this function.
 fn is_a_color_word(attempt: &str) -> bool {
-    attempt == "green" || attempt == "blue" || attempt == "red"
+    match attempt {
+        "green" | "blue" | "red" => true,
+        _ => false
+    }
 }
 
 fn main() {
     let word = String::from("green"); // Don't change this line.
 
-    if is_a_color_word(word) {
+    if is_a_color_word(&word) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
